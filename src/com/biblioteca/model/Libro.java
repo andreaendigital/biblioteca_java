2c0cfeb13b065c0c1697f3503e3ee20dd6429eef
Andrea Rosero <andrea.rosero.p@gmail.com>
2025-06-15 12:05:37 -0400

    creación clase libro

--- src/com/biblioteca/model/Libro.java
+++ src/com/biblioteca/model/Libro.java
@@ -9,5 +9,32 @@ package com.biblioteca.model;
  * @author Andrea
  */
 public class Libro {
-    
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private boolean prestado;
+
+    public Libro(String titulo, String autor, String isbn) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.prestado = false;
+    }
+
+    // Getters
+    public String getTitulo() { return titulo; }
+    public String getAutor() { return autor; }
+    public String getIsbn() { return isbn; }
+    public boolean isPrestado() { return prestado; }
+
+    // Setter
+    public void setPrestado(boolean prestado) { this.prestado = prestado; }
+
+    @Override
+    public String toString() {
+        return "Título: '" + titulo + '\'' +
+               ", Autor: '" + autor + '\'' +
+               ", ISBN: '" + isbn + '\'' +
+               ", Estado: " + (prestado ? "Prestado" : "Disponible");
+    }
 }
