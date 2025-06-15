6d4bfb1a3f03cb1b669a183dafba82a086447078
Andrea Rosero <andrea.rosero.p@gmail.com>
2025-06-15 12:03:13 -0400

    creación de estructura inicial

---
+++ src/com/biblioteca/ui/MenuPrincipal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.biblioteca.ui;
+
+/**
+ *
+ * @author Andrea
+ */
+public class MenuPrincipal {
+    //Contiene la lógica de interacción con el usuario, menús, etc
+}
